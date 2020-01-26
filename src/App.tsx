ba8384ec5c84a6695d3b954448119b13870648e3
Hollyhastoes <hollyhastoes@gmail.com>
2020-01-25 22:05:57 -0600

    I had so much fun today!!!

--- src/App.tsx
+++ src/App.tsx
@@ -4,36 +4,21 @@ import "./App.css";
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
+import { Router } from "react-router-dom";
+import { createBrowserHistory } from "history";
+import Routes from "./Routes";
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1,
-    textAlign: "center"
-  },
-  button: {
-    padding: theme.spacing(2),
-    color: theme.palette.text.secondary
-  }
-}));
+const browserHistory = createBrowserHistory();
+
+const useStyles = makeStyles(theme => ({}));
 
 export const App: React.FC = () => {
   const classes = useStyles();
   return (
     <Container>
-      <div className={classes.root}>
-        <Grid container spacing={3}>
-          <Grid item xs={12}>
-            <h1>I'm Serious As A Heart Attack</h1>
-            <Button
-              className="classes.button"
-              variant="contained"
-              color="primary"
-            >
-              Get Started, Dumb Ass
-            </Button>
-          </Grid>
-        </Grid>
-      </div>
+      <Router history={browserHistory}>
+        <Routes />
+      </Router>
     </Container>
   );
 };
---
+++ src/CategorySelection.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { createUseStyles } from "react-jss";
+import clsx from "clsx";
+
+const useStyles = createUseStyles({
+  trafficLight: {
+    width: "50px",
+    height: "50px",
+    borderRadius: "50px",
+    border: "black solid 1px",
+    backgroundColor: "white",
+    display: "inline-block",
+    fontFamily: "monospace"
+  },
+  red: { color: "red" }
+});
+
+export const CategorySelection: React.FC = () => {
+  const classes = useStyles();
+  return <div>Category Selections</div>;
+};
