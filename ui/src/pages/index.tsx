ec8a720d4902f5374915fd18e3f8dbb31a856f6c
Franklin Davenport <sparebytes@gmail.com>
2020-02-06 21:28:15 -0600

    amigo

--- ui/src/pages/index.tsx
+++ ui/src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Layout from "../components/Layout";
 import React from "react";
+import AmigoHome from "../AmigoHome";
 
 export default function Index() {
-  return <Layout>Hello</Layout>;
+  return <AmigoHome />;
 }
